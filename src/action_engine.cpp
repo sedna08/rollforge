0b86def21b48f75afc2555d2fe5cd46c2a94d5e3
sedna08 <ochiamitcham@gmail.com>
2026-03-26 14:35:28 +0800

    Changes: - Added action engine implementation and tests cases - Updated Cmake accordingly - Updated bindings.cpp for packaging this as a python library

--- src/action_engine.cpp
+++ src/action_engine.cpp
@@ -1,1 +1,23 @@
-// Stub: will be implemented in Phase 5
+#include "rollforge/action_engine.h"
+
+namespace rollforge {
+
+bool ActionResolver::ResolveAttack(const PlayerCharacter& attacker, const Entity& target, StatType stat) {
+    int attack_roll = attacker.PerformSkillCheck(stat);
+    return attack_roll >= target.armor_class;
+}
+
+void ActionResolver::ApplyDamage(Entity& target, int damage_amount) {
+    target.TakeDamage(damage_amount);
+}
+
+bool ActionResolver::AttemptLockpick(SessionState& state, int difficulty_class) {
+    int check_result = state.player.PerformSkillCheck(StatType::Dexterity);
+    if (check_result >= difficulty_class) {
+        state.world_flags["door_unlocked"] = true;
+        return true;
+    }
+    return false;
+}
+
+} // namespace rollforge
